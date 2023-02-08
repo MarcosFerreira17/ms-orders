a51cc108d1c0030a55a73ef9a407b3d88aafcabf
mnia <marcos.ferreira@gft.com>
2023-02-08 11:41:55 -0300

    adicionando mais dtos e preparando pareceber mais endpoints

--- src/main/java/com/mnia/orderservice/api/v1/controller/OrderController.java
+++ src/main/java/com/mnia/orderservice/api/v1/controller/OrderController.java
@@ -1,8 +1,11 @@
 package com.mnia.orderservice.api.v1.controller;
 
-import com.mnia.orderservice.domain.dtos.OrderDetailDto;
+import com.mnia.orderservice.domain.dtos.order.OrderDetailDTO;
 import com.mnia.orderservice.domain.services.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,9 +17,18 @@ public class OrderController {
     @Autowired
     private OrderService orderService;
 
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<Object> getOrder(@PageableDefault(page = 0, size = 10, sort = "id", direction = Sort.Direction.ASC)
+                                               Pageable pageable){
+        final var pageOrdersList = orderService.getOrderList(pageable);
+
+        return ResponseEntity.ok(HttpStatus.OK);
+    }
+
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public ResponseEntity<Object> placeOrder(@RequestBody OrderDetailDto orderDetailDto){
+    public ResponseEntity<Object> placeOrder(@RequestBody OrderDetailDTO orderDetailDto){
         orderService.placeOrder(orderDetailDto);
         return ResponseEntity.status(HttpStatus.CREATED).body("Order created Successfully");
     }
